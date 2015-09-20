e5c9e8f278f3d6aab2874dbd444a371b78b71dbb
Patrick B <patosaur@web.de>
2015-09-20 19:23:18 +0200

    added reference class

---
+++ gw2tools/src/main/java/com/runemagic/gw2tools/reference/Reference.java
@@ -0,0 +1,18 @@
+package com.runemagic.gw2tools.reference;
+
+
+public class Reference
+{
+    private Reference()
+    {
+
+    }
+
+
+    /**
+     * Registry paths
+     */
+    public static final String REGISTRY_ROOT = "gw2tools";
+    public static final String REGISTRY_WINDOW_BOUNDS = REGISTRY_ROOT + "/window_bounds";
+
+}
