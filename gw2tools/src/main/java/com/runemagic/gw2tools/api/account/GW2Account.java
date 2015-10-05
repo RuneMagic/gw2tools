3c2c4a35ef1b683d9275491d3be1729719ff522f
faelar <autistic.batman@gmail.com>
2015-10-05 15:52:21 +0200

    Account placeholder properties and UI (WIP)

--- gw2tools/src/main/java/com/runemagic/gw2tools/api/account/GW2Account.java
+++ gw2tools/src/main/java/com/runemagic/gw2tools/api/account/GW2Account.java
@@ -1,12 +1,18 @@
 package com.runemagic.gw2tools.api.account;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyListProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 
 import org.json.JSONArray;
@@ -22,7 +28,11 @@ public class GW2Account extends AuthenticatedAPIObject
 	private final static String API_RESOURCE_CHARACTERS="characters";
 
 	private ListProperty<GW2Character> characters=new SimpleListProperty<>(FXCollections.observableArrayList());//TODO unmodifiable list
-	//TODO more
+	private StringProperty id=new SimpleStringProperty();
+	private StringProperty name=new SimpleStringProperty();
+	private ObjectProperty<World> world=new SimpleObjectProperty<>();
+	private ListProperty<Guild> guilds=new SimpleListProperty<>(FXCollections.observableArrayList());//TODO unmodifiable list
+	private ObjectProperty<Instant> created=new SimpleObjectProperty<>();
 
 	public GW2Account(GW2APISource source, APIKey apiKey)
 	{
@@ -45,7 +55,7 @@ public class GW2Account extends AuthenticatedAPIObject
 
 	@Override protected void updateImpl() throws GW2APIException
 	{
-		List<String> charNames=new ArrayList<String>();
+		List<String> charNames=new ArrayList<>();
 		JSONArray json = new JSONArray(readAPIv2Resource(API_RESOURCE_CHARACTERS, this));
 		int len=json.length();
 		for (int i=0;i<len;i++)
@@ -79,5 +89,52 @@ public class GW2Account extends AuthenticatedAPIObject
 			character.waitForUpdate();
 			progress(inc);
 		}
+
+		//TODO the rest
+	}
+
+	public String getId()
+	{
+		return id.get();
+	}
+
+	public StringProperty idProperty()
+	{
+		return id;
+	}
+
+	public String getName()
+	{
+		return name.get();
+	}
+
+	public StringProperty nameProperty()
+	{
+		return name;
+	}
+
+	public World getWorld()
+	{
+		return world.get();
+	}
+
+	public ObjectProperty<World> worldProperty()
+	{
+		return world;
+	}
+
+	public ReadOnlyListProperty<Guild> getGuilds()
+	{
+		return guilds;
+	}
+
+	public Instant getCreated()
+	{
+		return created.get();
+	}
+
+	public ReadOnlyObjectProperty<Instant> createdProperty()
+	{
+		return created;
 	}
 }
